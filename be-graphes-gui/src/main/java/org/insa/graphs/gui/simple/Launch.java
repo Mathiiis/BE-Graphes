13fd34aa236bca00f1f4f17117b0d7c755df6e4b
Efe Inkaya <efeinkaya@gmail.com>
2025-04-11 08:51:42 +0200

    Launch.java done

--- be-graphes-gui/src/main/java/org/insa/graphs/gui/simple/Launch.java
+++ be-graphes-gui/src/main/java/org/insa/graphs/gui/simple/Launch.java
@@ -14,6 +14,7 @@ import org.insa.graphs.gui.drawing.components.BasicDrawing;
 import org.insa.graphs.model.Graph;
 import org.insa.graphs.model.Path;
 import org.insa.graphs.model.io.BinaryGraphReader;
+import org.insa.graphs.model.io.BinaryPathReader;
 import org.insa.graphs.model.io.GraphReader;
 import org.insa.graphs.model.io.PathReader;
 
@@ -46,9 +47,9 @@ public class Launch {
 
         // visit these directory to see the list of available files on commetud.
         final String mapName =
-                "/mnt/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Maps/insa.mapgr";
+                "../Maps/insa.mapgr";
         final String pathName =
-                "/mnt/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Paths/path_fr31insa_rangueil_r2.path";
+                "../Maps/path_fr31insa_rangueil_r2.path";
 
         final Graph graph;
         final Path path;
@@ -57,23 +58,22 @@ public class Launch {
         try (final GraphReader reader = new BinaryGraphReader(new DataInputStream(
                 new BufferedInputStream(new FileInputStream(mapName))))) {
 
-            // TODO: read the graph
-            graph = null;
+            graph = reader.read();
         }
 
         // create the drawing
         final Drawing drawing = createDrawing();
 
-        // TODO: draw the graph on the drawing
+        drawing.drawGraph(graph);
 
-        // TODO: create a path reader
-        try (final PathReader pathReader = null) {
+        try (final PathReader pathReader = new BinaryPathReader(
+            new DataInputStream(new BufferedInputStream(
+                    new FileInputStream(pathName)))) ) {
 
-            // TODO: read the path
-            path = null;
+            path = pathReader.readPath(graph);
         }
 
-        // TODO: draw the path on the drawing
+        drawing.drawPath(path);
     }
 
 }
